8e8b60b1ae0393b8d1f9d63027d77fc540c7fe61
Alexis <alexislui@hotmail.co.uk>
2016-11-21 15:31:12 +0000

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -17,7 +17,7 @@ function moveslider(n) {
 	x[sliderIndex-1].style.display = "block";		// shows the image according to sliderIndex
 }
 
-// SMOOTH SCROLLING - Pure Javascript - Source: http://web.archive.org/web/20140103045600/http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript 
+// SMOOTH SCROLLING - Pure Javascript - Credit: http://web.archive.org/web/20140103045600/http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript 
 
 function currentYPosition() {
     if (window.pageYOffset) return window.pageYOffset; // pageXOffset and pageYOffset properties returns the pixels the current document has been scrolled from the upper left corner of the window, horizontally and vertically
@@ -36,14 +36,14 @@ function elmYPosition(eID) {
 function smoothScroll(eID) {
     var startY = currentYPosition();
     var stopY = elmYPosition(eID);
-    var distance = stopY > startY ? stopY - startY : startY - stopY;
+    var distance = stopY > startY ? stopY - startY : startY - stopY;	// option for bi-directional scrolling
     if (distance < 10) {
-        scrollTo(0, stopY); return;
+        scrollTo(0, stopY); return;					// does not scroll if too close
     }
-    var speed = Math.round(distance / 100);
-    if (speed >= 20) speed = 20;
+    var speed = Math.round(distance / 100);			// speed if faster is destination is further away
+    if (speed >= 20) speed = 20;					
     var step = Math.round(distance / 25);
-    var leapY = stopY > startY ? startY + step : startY - step;
+    var leapY = stopY > startY ? startY + step : startY - step;	// increments to leap after every delay
     var timer = 0;
     if (stopY > startY) {
         for ( var i=startY; i<stopY; i+=step ) {
